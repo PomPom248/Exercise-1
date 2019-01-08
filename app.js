4c522a35fd3a32d7e06020939528f174e8c2cc90
PomPom248 <sofiroggero@gmail.com>
2019-01-08 17:59:52 +0100

    iteration 5

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
-
-app.use('/', require('./server/index'));
-// app.use('/', require('./server/message'));
+app.use('/', require('./server/index'))
 
 app.listen(9001, () => { console.log('listening to port 9001') })
 module.exports = app;
--- server/message.js
+++ server/message.js
@@ -1,36 +1,15 @@
 const express = require('express');
-const messageRouter = express.Router();
-const axios = require('axios')
-
-const axiosRoute = axios.create({
-    baseURL: "http://exercise-1_messageapp_1:3000"
-})
+const messageRouter = express.Router()
+const messageFunction = require('./messageFunction')
 
 messageRouter.post('/messages', (req, res, next) => {
     const { destination, body } = req.body
-    sendMessage(destination, body, res)
-})
-
-sendMessage = (destination, body, res) => {
-    return axiosRoute.post("/message", { destination, body })
-        .then((response) => {
-            console.log(response)
+    messageFunction.sendMessage(destination, body)
+        .then(() => {
             res.status(200).json({ message: 'Message created' })
         })
-        .catch((err) => {
-            console.log(err)
-            res.status(500)
+        .catch(() => {
+            res.status(500).json({ message: 'Error while creating the message' })
         })
-}
-
-// messageRouter.get('/', (req, res, next) => {
-//     return axiosRoute.get(baseURL)
-//         .then(() => {
-//             res.status(200).json()
-//         })
-//         .catch((err) => {
-//             res.status(500).json(err)
-//         })
-// })
-
+})
 module.exports = messageRouter;
